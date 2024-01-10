40b9187419237aa307ed5acc1cb904741255d950
SeroTomas <serocontacto@gmail.com>
2024-01-09 21:00:55 -0300

    section finished

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				"c-darkMagenta": "hsl(300, 43%, 22%)",
+				"c-softPink": "hsl(333, 80%, 67%)",
+				"c-dGMagenta": "hsl(303, 10%, 53%)",
+				"c-lGMagenta": "hsl(300, 24%, 96%)",
+			},
+			fontFamily: {
+				"c-spartan": ['League Spartan', 'sans-serif']
+			},
+		},
 	},
 	plugins: [],
 }
